e92f10d1fd2ec41698a8f81bfe58a2e7d006720f
Aarthipriya A <a.aarthipriya2021@gmail.com>
2023-09-14 20:45:27 +0530

    mui rating

--- src/components/08_MuiRating.jsx
+++ src/components/08_MuiRating.jsx
@@ -1,22 +1,82 @@
-import React, {useState} from 'react';
-import { Box, Typography, Rating } from '@mui/material';
+import { useState } from "react";
+import { Box, Rating, Typography } from "@mui/material";
+import StarIcon from "@mui/icons-material/Star";
 
+const labels = {
+  0.5: "Useless",
+  1: "Useless+",
+  1.5: "Poor",
+  2: "Poor+",
+  2.5: "Ok",
+  3: "Ok+",
+  3.5: "Good",
+  4: "Good+",
+  4.5: "Excellent",
+  5: "Excellent+",
+};
 
-const MuiRating = () => {
-
-  const [value, setValue] = useState(2)
-  return (
-    <Box mt={4}>
-      <Typography>Controlled</Typography>
-      <Rating 
-        name='controlled_rating'
-        value={value}
-        onChange={(e, newValue) => {
-          setValue(newValue);
-        }}
-      />
-    </Box>
-  )
+function getLabelText(value) {
+  return `${value} Star${value !== 1 ? "s" : ""}, ${labels[value]}`;
 }
 
-export default MuiRating;
+export default function MuiRating() {
+  const [value, setValue] = useState(2);
+  const [hover, setHover] = useState(-1);
+
+  return (
+    <Box>
+      <Box mt={4}>
+        <Typography>Controlled</Typography>
+        <Rating
+          name="controlled_rating"
+          value={value}
+          onChange={(e, newValue) => {
+            setValue(newValue);
+          }}
+        />
+
+        <Typography>Disabled</Typography>
+        <Rating name="Disabled" value={value} disabled />
+
+        <Typography>Read-only</Typography>
+        <Rating name="read-only" value={value} read-only />
+
+        <Typography>No rating given</Typography>
+        <Rating name="no-value" value={null} />
+      </Box>
+
+      {/* displying the precision value */}
+      <Box mt={4}>
+        <Typography>Rating Precision</Typography>
+        <Rating
+          name="controlled_rating"
+          precision={0.5}
+          value={value}
+          onChange={(e, newValue) => {
+            setValue(newValue);
+          }}
+        />
+      </Box>
+
+      {/* feedback feature  */}
+      <Rating
+        name="hover-feedback"
+        value={value}
+        precision={0.5}
+        getLabelText={getLabelText}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+        onChangeActive={(event, newHover) => {
+          setHover(newHover);
+        }}
+        emptyIcon={<StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />}
+      />
+      {value !== null && (
+        <Box sx={{ ml: 2 }}>{labels[hover !== -1 ? hover : value]}</Box>
+      )}
+    </Box>
+  );
+}
+
+// export default MuiRating;
